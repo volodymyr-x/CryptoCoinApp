723fd9e4a281028ec1d4ebc1a9f4e5009332aa78
Volodymyr Khmelnytskiy <igel4@ukr.net>
2025-05-14 21:27:25 +0300

    Removed unused code.

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,9 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_application_1/router/router.gr.dart';
 
-import '../features/currency_info/view/currency_info.dart';
-import '../features/currency_list/view/currency_list.dart';
-
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
 
